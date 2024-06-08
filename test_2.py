c3495809c8f5ea0ea7ab17f9a9240d94ea2c86cb
Дмитрий Иноземцев <Dmitriy-inz@yandex.ru>
2024-06-08 20:02:02 +0300

    new_commit_22

--- test_2.py
+++ test_2.py
@@ -15,7 +15,7 @@ user_info = {'first_name': 'Алёна', 'last_name': 'Петрова', 'age': 2
 full_name = get_full_name(user_info)
 print(f'Здравствуйте, {full_name}')
 
-if has_full_access(user_info) is True
+if: has_full_access(user_info) is True
     print('Вам есть 18 лет и у вас есть доступ ко всем возможностям сайта.')
 else:
     print('Вам нет 18 лет, ваш доступ ограничен.')
